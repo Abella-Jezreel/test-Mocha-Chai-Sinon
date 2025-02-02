5817e3f345e4c161cfaaf260961f6beb315a4424
Abella-Jezreel <teamabella@outlook.com>
2025-02-02 15:45:53 +0800

    Testing Code That Requires Authentication

--- test/auth-controller.js
+++ test/auth-controller.js
@@ -52,4 +52,81 @@ describe("Auth Controller", function () {
 
     User.findById.restore();
   });
+
+  it("should update the user status", async function () {
+    const req = {
+      userId: "5f447132b4f3f6a7b8e9f2e7",
+      body: {
+        status: "I am updated!"
+      }
+    };
+    const res = {
+      statusCode: 500,
+      message: null,
+      status: function (code) {
+        this.statusCode = code;
+        return this;
+      },
+      json: function (data) {
+        this.message = data.message;
+      }
+    };
+
+    sinon.stub(User, "findById").resolves({
+      status: "I am new!",
+      save: sinon.stub().resolves()
+    });
+
+    await authController.updateUserStatus(req, res, () => {});
+
+    expect(res.statusCode).to.be.equal(200);
+    expect(res.message).to.be.equal("User updated.");
+    expect(User.findById.called).to.be.true;
+
+    User.findById.restore();
+  });
+
+  it("should return 404 if user is not found", async function () {
+    const req = {
+      userId: "5f447132b4f3f6a7b8e9f2e7",
+      body: {
+        status: "I am updated!"
+      }
+    };
+    const res = {};
+    const next = sinon.spy();
+
+    sinon.stub(User, "findById").resolves(null);
+
+    await authController.updateUserStatus(req, res, next);
+
+    expect(next.calledOnce).to.be.true;
+    const error = next.firstCall.args[0];
+    expect(error).to.be.an("error");
+    expect(error).to.have.property("statusCode", 404);
+
+    User.findById.restore();
+  });
+
+  it("should handle errors correctly", async function () {
+    const req = {
+      userId: "5f447132b4f3f6a7b8e9f2e7",
+      body: {
+        status: "I am updated!"
+      }
+    };
+    const res = {};
+    const next = sinon.spy();
+
+    sinon.stub(User, "findById").throws();
+
+    await authController.updateUserStatus(req, res, next);
+
+    expect(next.calledOnce).to.be.true;
+    const error = next.firstCall.args[0];
+    expect(error).to.be.an("error");
+    expect(error).to.have.property("statusCode", 500);
+
+    User.findById.restore();
+  });
 });
